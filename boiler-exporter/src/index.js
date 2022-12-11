6ad32c6c85400095f4ac576376ca537a400a4f32
psmolenski <pwl.smolenski@gmail.com>
2022-12-11 18:41:54 +0100

    Added boiler-exporter

---
+++ boiler-exporter/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const axios = require('axios')
+
+const app = express()
+const port = 80
+
+app.get('/boiler-metrics', async (req, res) => {
+    const response = await axios.get('http://192.168.10.56/api/boiler/')
+    const metrics = Object.entries(response.data)
+        .map(([propName, propValue]) => {
+            return `${propName} ${propValue}`
+        })
+        .join('\n');
+
+    res.set('Content-Type', 'text/plain')
+    res.send(metrics)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
