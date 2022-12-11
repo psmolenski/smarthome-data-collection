8400c2f91c49502da02510aa8493e2a442b92135
psmolenski <pwl.smolenski@gmail.com>
2022-12-11 19:01:26 +0100

    Added quotation marks to metric values

--- boiler-exporter/src/index.js
+++ boiler-exporter/src/index.js
@@ -8,7 +8,7 @@ app.get('/metrics', async (req, res) => {
     const response = await axios.get('http://192.168.10.56/api/boiler/')
     const metrics = Object.entries(response.data)
         .map(([propName, propValue]) => {
-            return `${propName} ${propValue}`
+            return `${propName} "${propValue}"`
         })
         .join('\n');
 
