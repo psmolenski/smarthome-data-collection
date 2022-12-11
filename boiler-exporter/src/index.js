4ed9aa156ae39bd747e64faacd31f647b1f56cd1
psmolenski <pwl.smolenski@gmail.com>
2022-12-11 19:23:19 +0100

    Added boiler metrics filtering

--- boiler-exporter/src/index.js
+++ boiler-exporter/src/index.js
@@ -4,9 +4,14 @@ const axios = require('axios')
 const app = express()
 const port = 80
 
+const displayedMetrics = ['outdoortemp', 'curflowtemp', 'selflowtemp', 'heatingtemp']
+
 app.get('/metrics', async (req, res) => {
     const response = await axios.get('http://192.168.10.56/api/boiler/')
     const metrics = Object.entries(response.data)
+        .filter(([propName]) => {
+            return displayedMetrics.includes(propName)
+        })
         .map(([propName, propValue]) => {
             return `${propName} ${propValue}`
         })
